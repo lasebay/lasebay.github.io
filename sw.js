dc11cc0efba0d6ba93e84f757ee2c65b591dc6ba
Bayudan <lasbay@gmail.com>
2021-04-26 15:37:24 -0400

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-802e85a99b13a991a812.js"
+    "url": "webpack-runtime-f8fdc42e5130979c485c.js"
   },
   {
     "url": "framework-cd3e1e804d552fa282ef.js"
@@ -37,7 +37,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "8476ed6262cbc6a3a2340004fcab3463"
+    "revision": "7f80a22f5b9a9981f4f2e63deec75a30"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-b0556ce5127c1a3e2490.js"
